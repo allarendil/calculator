7f5f68d5d14d4eee0bacc7547f7bd8d81853aaed
allarendil <tebicat516@gmail.com>
2023-11-14 14:18:47 -0500

    Add functions and styling to complete calculator

--- main.js
+++ main.js
@@ -1,182 +1,96 @@
-const app = document.getElementById('app');
-
-const calcData = [
-    { id: "clear", value: "AC" },
-    { id: "divide", value: "/" },
-    { id: "multiply", value: "x" },
-    { id: "seven", value: "7" },
-    { id: "eight", value: "8" },
-    { id: "nine", value: "9" },
-    { id: "subtract", value: "-" },
-    { id: "four", value: "4" },
-    { id: "five", value: "5" },
-    { id: "six", value: "6" },
-    { id: "add", value: "+" },
-    { id: "one", value: "1" },
-    { id: "two", value: "2" },
-    { id: "three", value: "3" },
-    { id: "equals", value: "=" },
-    { id: "zero", value: "0" },
-    { id: "decimal", value: "." }
-];
-
-const operators = ["AC", "/", "x", "+", "-", "="];
-
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-const Display = ({ input, output }) => (
-    <div className="output">
-        <span className="result">{output}</span>
-        <span id="display" className="input">{input}</span>
-    </div>
-);
-
-const Key = ({ keyData: { id, value }, handleInput }) => (
-    <button id={id} onClick={() => handleInput(value)}>
-        {value}
-    </button>
-);
-
-const Keyboard = ({ handleInput }) => (
-    <div className="keys">
-        {calcData.map((key) => (
-            <Key key={key.id} keyData={key} handleInput={handleInput}/>
-        ))}
-    </div>
-);
-
-const App = () => {
-    const [input, setInput] = "0";
-    const [output, setOutput] = "";
-    const [calculatorData, setCalculatorData] = "";
-
-    const handleSubmit = () => {
-        const total = eval(calculatorData);
-        setInput(total);
-        setOutput(`${total} = ${total}`);
-        setCalculatorData(`${total}`);
-    };
-
-    const handleClear = () => {
-        setInput("0");
-        setCalculatorData("");
-    };
-
-    const handleNumbers = (value) => {
-        if (!calculatorData.length) {
-            setInput(`${value}`);
-            setCalculatorData(`${value}`);
-        } else {
-            if (value === 0 && (calculatorData === "0" || input === "0")) {
-                setCalculatorData(`${calculatorData}`);
-              } else {
-                const lastChat = calculatorData.charAt(calculatorData.length - 1);
-                const isLastChatOperator =
-                  lastChat === "*" || operators.includes(lastChat);
-                  setInput(isLastChatOperator ? `${value}` : `${input}${value}`);
-                  setCalculatorData(`${calculatorData}${value}`);
-        }
-    }
-
-};
+let operator = '';
+let previousValue = '';
+let currentValue = '';
 
 
-const dotOperator = () => {
-    const lastChat = calculatorData.charAt(calculatorData.length - 1);
-    if (!calculatorData.length) {
-      setInput("0.");
-      setCalculatorData("0.");
+document.addEventListener("DOMContentLoaded", function(){
+  let clear = document.querySelector(".clear");
+  let equal = document.querySelector(".equal");
+  let decimal = document.querySelector(".decimal");
+
+  let numbers = document.querySelectorAll(".number");
+  let operators = document.querySelectorAll(".operator");
+
+  let previousScreen = document.querySelector(".previous");
+  let currentScreen = document.querySelector(".current");
+
+  numbers.forEach((number) => number.addEventListener("click", function(e){
+    handleNumber(e.target.textContent)
+    currentScreen.textContent = currentValue;
+  }))
+
+  operators.forEach((op) => op.addEventListener("click", function(e){
+    handleOperator(e.target.textContent)
+    previousScreen.textContent = previousValue + " " + operator;
+    currentScreen.textContent = currentValue;
+  }))
+
+  clear.addEventListener("click", function(){
+    previousValue = '';
+    currentValue = '';
+    operator = '';
+    previousScreen.textContent = currentValue;
+    currentScreen.textContent = currentValue;
+  })
+
+  equal.addEventListener("click", function(){
+    if (currentValue != '' && previousValue != '') {
+    calculate()
+    previousScreen.textContent = '';
+    if (previousValue.length <= 5){
+      currentScreen.textContent = previousValue;
     } else {
-      if (lastChat === "*" || operators.includes(lastChat)) {
-        setInput("0.");
-        setCalculatorData(`${calculatorData} 0.`);
-      } else {
-        setInput(
-        lastChat === "." || input.includes(".") ? `${input}` : `${input}.`
-        );
-        const formattedValue =
-              lastChat === "." || input.includes(".")
-              ? `${calculatorData}`
-              : `${calculatorData}.`;
-        setCalculatorData(formattedValue);
-      }
+      currentScreen.textContent = previousValue.slice(0,5) + "...";
     }
-  };
-
-
-  const handleOperators = (value) => {
-    if (calculatorData.length) {
-      setInput(`${value}`);
-      const beforeLastChat = calculatorData.charAt(calculatorData.length - 2);
-
-      const beforeLastChatIsOperator =
-        operators.includes(beforeLastChat) || beforeLastChat === "*";
-
-      const lastChat = calculatorData.charAt(calculatorData.length - 1);
-      
-      const lastChatIsOperator = operators.includes(lastChat) || lastChat === "*";
-      
-      const validOp = value === "x" ? "*" : value;
-      if (
-        (lastChatIsOperator && value !== "-") ||
-        beforeLastChatIsOperator && lastChatIsOperator
-      ) {
-        if (beforeLastChatIsOperator) {
-          const updatedValue = `${calculatorData.substring(
-            0,
-            calculatorData.length - 2
-          )}${value}`;
-          setCalculatorData(updatedValue);
-        } else {
-          setCalculatorData(`${calculatorData.substring(0, calculatorData.length - 1)}${validOp}`);
-        }
-      } else {
-        setCalculatorData(`${calculatorData}${validOp}`);
-      }
-    }
-  };
-  const handleInput = (value) => {
-    const number = numbers.find((num) => num === value);
-    const operator = operators.find((op) => op === value);
+  }
     
-    switch(value) {
-      case "=":
-        handleSubmit();
-        break;
-      case "AC":
-        handleClear();
-        break;
-      case number:
-        handleNumbers(value);
-        break;
-      case ".":
-        dotOperator();
-        break;
-      case operator:
-        handleOperators(value);
-        break;
-      default:
-        break;
-    }
-  }
-  
-  const handleOutput = () => {
-    setOutput(calculatorData)
-  }
-  
- let permit = (() => {
-    handleOutput();
-  }, [calculatorData]);
+  })
 
-  permit();
+  decimal.addEventListener("click", function() {
+    addDecimal();
+  })
 
-  return (
-      <div className="container">
-      <div className="calculator">
-      <Display input={input} output={output}/>
-      <Keyboard handleInput={handleInput} />
-      </div>
-      </div>
-);
+})
+
+function handleNumber(number){
+  if(currentValue.length <= 5) {
+  currentValue += number;
+  }
 };
+
+function handleOperator(op){
+  operator = op;
+  previousValue = currentValue;
+  currentValue = '';
+};
+
+function calculate(){
+  previousValue = Number(previousValue);
+  currentValue = Number(currentValue);
+
+  if (operator === "+"){
+    previousValue += currentValue;
+  } else if (operator === "-") {
+    previousValue -= currentValue;
+  } else if (operator === "x"){
+    previousValue *= currentValue;
+  } else {
+    previousValue /= currentValue;
+  }
+
+  previousValue = roundNumber(previousValue);
+  previousValue = previousValue.toString();
+  currentValue = currentValue.toString();
+} 
+
+function roundNumber(num) {
+  return Math.round(num * 1000) / 1000;
+}
+
+function addDecimal() {
+  if (!currentValue.includes(".")){
+    currentValue += ".";
+  }
+}
+
 
